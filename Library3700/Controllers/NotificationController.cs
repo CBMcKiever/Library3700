a1eaa0f27dd2a6abbd8fe9abbfb1000b9aef5f0b
Will Coughlin <willcoughlin@gmail.com>
2018-04-23 09:44:27 -0500

    1. Created controllers; 2. Began implementing notification system

---
+++ Library3700/Controllers/NotificationController.cs
@@ -0,0 +1,29 @@
+﻿using Library3700.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library3700.Controllers
+{
+    public class NotificationController : Controller
+    {
+        // GET: Notification
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /* 
+         * Class model methods
+         */
+
+        public Notification CreateNotification(string text) => new Notification(text);
+
+        public void SendNotification(Notification notification)
+        {
+            // TODO: implement
+        }
+    }
+}
---
+++ Library3700/Models/ViewModels/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library3700.Models.ViewModels
+{
+    public class Notification
+    {
+        private static int _count;
+
+        private int _id;
+        private string _text;
+
+        static Notification()
+        {
+            _count = 0;
+        }
+
+        public Notification(string text)
+        {
+            _id = _count++;
+            _text = text;
+        }
+
+        public int Id { get; }
+
+        public string GetNotification => _text;
+    }
+}
