9b0fde2229d9b170509b12bb33289d2a9f968ae5
Will Coughlin <willcoughlin@gmail.com>
2018-04-26 17:00:01 -0500

    Reinstall Owin

--- Library3700/Startup.cs
+++ Library3700/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Builder;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
 
@@ -10,7 +11,7 @@ namespace Library3700
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration(AppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
 
