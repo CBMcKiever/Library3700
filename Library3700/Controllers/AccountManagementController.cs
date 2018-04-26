2637ffb5022972dad9467b09b585bf87984a1118
Will Coughlin <willcoughlin@gmail.com>
2018-04-26 16:31:50 -0500

    Add user login and registration

--- Library3700/Controllers/AccountManagementController.cs
+++ Library3700/Controllers/AccountManagementController.cs
@@ -1,17 +1,170 @@
-﻿using System;
+﻿using Library3700.Models.Objects.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Claims;
+using Library3700.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Library3700.Controllers
 {
     public class AccountManagementController : Controller
     {
+        private AccountAdapter activeAccount;
+
+        public class RegisterRequest
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string EmailAddress { get; set; }
+
+            public bool IsLibrarian { get; set; }
+        }
+
         // GET: AccountManagement
-        public ActionResult Index()
+        public ActionResult Home()
+        {
+            ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+
+            if (User.IsInRole("librarian"))
+            {
+                activeAccount = new Librarian
+                {
+                    LibrarianEmailAddress = claims.Where(x => x.Type == ClaimTypes.Email).Single().Value,
+                    LibrarianFirstName = claims.Where(x => x.Type == ClaimTypes.GivenName).Single().Value,
+                    LibrarianLastName = claims.Where(x => x.Type == ClaimTypes.Surname).Single().Value,
+                    LibrarianId = Int32.Parse(claims.Where(x => x.Type == ClaimTypes.UserData).Single().Value)
+                };
+            }
+            else if (ClaimsPrincipal.Current.IsInRole("patron"))
+            {
+                activeAccount = new Patron
+                {
+                    PatronEmailAddress = claims.Where(x => x.Type == ClaimTypes.Email).Single().Value,
+                    PatronFirstName = claims.Where(x => x.Type == ClaimTypes.GivenName).Single().Value,
+                    PatronLastName = claims.Where(x => x.Type == ClaimTypes.Surname).Single().Value,
+                    PatronId = Int32.Parse(claims.Where(x => x.Type == ClaimTypes.UserData).Single().Value)
+                };
+            }
+            else
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            return View();
+        }
+        
+        /// <summary>
+        /// Display add account page.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddAccount()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Handle form request to register new user.
+        /// </summary>
+        /// <param name="request">Form data</param>
+        /// <returns></returns>
+        public JsonResult RegisterNewAccount(RegisterRequest request)
+        {
+            try
+            {
+                using (var db = new LibraryEntities())
+                {
+                    if (db.Logins.Where(x => x.Username == request.EmailAddress).Any())
+                    {
+                        throw new ApplicationException(
+                            "A user with the email address " + request.EmailAddress + " already exists!");
+                    }
+
+                    var newAccount = new Account
+                    {
+                        FirstName = request.FirstName,
+                        LastName = request.LastName,
+                        IsLibrarian = request.IsLibrarian
+                    };
+
+                    string temporaryPassword = GenerateTemporaryPassword();
+                    string temporaryPasswordHash = HashString(temporaryPassword);
+
+                    using (var transaction = db.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            db.Accounts.Add(newAccount);
+                            db.SaveChanges();
+
+                            var newLogin = new Login
+                            {
+                                AccountId = newAccount.AccountId,
+                                Username = request.EmailAddress,
+                                PasswordHash = temporaryPasswordHash,
+                                IsPasswordTemporary = true
+                            };
+
+                            db.Logins.Add(newLogin);
+                            db.SaveChanges();
+                            
+                            transaction.Commit();
+                        }
+                        catch (Exception e)
+                        {
+                            transaction.Rollback();
+                            throw e;
+                        }
+                    }
+
+                    return Json(new
+                    {
+                        Success = true,
+                        Message = "Created user with temporary password: " + temporaryPassword
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { Success = false, e.Message });
+            }
+        }
+
+        /// <summary>
+        /// Hash a string with SHA512
+        /// </summary>
+        /// <param name="source">String to hash</param>
+        /// <returns>SHA512 hash of string</returns>
+        private static string HashString(string source)
+        {
+            using (var sha512 = SHA512.Create())
+            {
+                byte[] bytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(source));
+
+                StringBuilder sb = new StringBuilder();
+                foreach (var b in bytes)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Generate a random 8 character alphanumeric password
+        /// </summary>
+        /// <returns>Password</returns>
+        private static string GenerateTemporaryPassword()
+        {
+            Random random = new Random();
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new String(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
--- Library3700/Controllers/LoginController.cs
+++ Library3700/Controllers/LoginController.cs
@@ -1,7 +1,10 @@
 ﻿using Library3700.Models;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -9,96 +12,188 @@ using System.Web.Mvc;
 
 namespace Library3700.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         /// <summary>
         /// A simple wrapper for login requests
         /// </summary>
         public class LoginRequest
-        {
+        { 
             public string Username { get; set; }
             public string PasswordHash { get; set; }
+            public string ReturnUrl { get; set; }
         }
 
         // GET: Login
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string returnUrl)
         {
-            return View();
+            // don't show login page to user already logged in
+            if (Request.IsAuthenticated)
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         /// <summary>
-        /// Handle request from login form, providing a cookie to an authenticated user or denying an invalid login.
+        /// Handle login form request.
         /// </summary>
-        /// <param name="loginRequest">Wrapper for URL parameters (Username, PasswordHash).</param>
-        /// <returns>Action based on result of authentication</returns>
-        public HttpStatusCodeResult Login(LoginRequest loginRequest)  // returning status code for testing purposes
-        // public ActionResult Login(LoginRequest loginRequest)
+        /// <param name="request">Login request form form data</param>
+        /// <returns>
+        /// Returns redirect to request's Return URL if authentication succesful;
+        /// otherwise, returns to login page.
+        /// </returns>
+        [HttpPost]
+        public ActionResult LogIn(LoginRequest request)
         {
-            // TODO: redirect to proper page
-
-            string username = loginRequest.Username;
-            string passwordHash = loginRequest.PasswordHash;
-
             try
             {
-                if (String.IsNullOrWhiteSpace(username) || String.IsNullOrWhiteSpace(passwordHash))
-                {
-                    // error: invalid parameter
-                    throw new ApplicationException("Value username and/or passwordHash is invalid!");
-                }
-
                 using (var db = new LibraryEntities())
                 {
-                    var targetLogin = db.Logins.Where(x => x.Username == username).FirstOrDefault();
+                    var targetLogin = db.Logins
+                        .Where(x => x.Username == request.Username && x.PasswordHash == request.PasswordHash)
+                        .SingleOrDefault();
+
                     if (targetLogin == null)
                     {
-                        // error: user not found
-                        throw new ApplicationException("User " + username + " not found!");
+                        throw new ApplicationException("The username or password is incorrect!");
                     }
 
-                    bool isCorrectPassword = targetLogin.PasswordHash == passwordHash;
-                    if (!isCorrectPassword)
+                    var userAccount = targetLogin.Account;
+
+                    var identity = new ClaimsIdentity(new[]
                     {
-                        // error: incorrect password
-                        throw new ApplicationException("Incorrect password!");
-                    }
+                        new Claim(ClaimTypes.GivenName, userAccount.FirstName),
+                        new Claim(ClaimTypes.Surname, userAccount.LastName),
+                        new Claim(ClaimTypes.Role, userAccount.IsLibrarian ? "librarian" : "patron"),
+                        new Claim(ClaimTypes.Email, targetLogin.Username),
+                        new Claim(ClaimTypes.UserData, userAccount.AccountId.ToString())
+                    },
+                    "ApplicationCookie");
 
-                    HttpCookie loginCookie = CreateCookie(targetLogin);
-                    Response.Cookies.Add(loginCookie);
+                    IOwinContext ctx = Request.GetOwinContext();
+                    IAuthenticationManager authMgr = ctx.Authentication;
 
-                    //return View();
-                    return new HttpStatusCodeResult(200);
+                    authMgr.SignIn(identity);
+
+                    return Redirect(GetRedirectUrl(request.ReturnUrl));
                 }
+            }
+            catch (ApplicationException e)
+            {
+                NotificationController.CreateNotification(e.Message);
+                // TODO: send notification
+                return View("Index");
             }
             catch (Exception e)
             {
-                // handle login error
-                return new HttpStatusCodeResult(500);
+                // TODO: show error page
+                return View("Index");
             }
         }
 
-        private HttpCookie CreateCookie(Login login)
+        public ActionResult LogOut()
         {
-            string cookieValue = String.Format("{0};{1};{2}", login.AccountId, login.LoginId, login.Username);
-            string cookieHash;
+            IOwinContext ctx = Request.GetOwinContext();
+            IAuthenticationManager authMgr = ctx.Authentication;
 
-            using (var sha512 = SHA512.Create())
+            authMgr.SignOut("ApplicationCookie");
+            return RedirectToAction("Index", "Login");
+        }
+
+        /// <summary>
+        /// Validates ReturnUrl for LoginRequest object.
+        /// </summary>
+        /// <param name="returnUrl">Value of ReturnUrl</param>
+        /// <returns>Request's return URL if set, homepage url otherwise</returns>
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
             {
-                byte[] cookieBytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(cookieValue));
-                var sb = new StringBuilder();
-                foreach (byte b in cookieBytes)
-                {
-                    sb.Append(b.ToString("x2"));
-                }
-                cookieHash = sb.ToString();
+                return Url.Action("Home", "AccountManagement");
             }
 
-            var cookie = new HttpCookie("auth", login.Username + ":" + cookieHash)
-            {
-                Expires = DateTime.Now.AddDays(1)
-            };
-
-            return cookie;
+            return returnUrl;
         }
+
+        ///// <summary>
+        ///// Handle request from login form, providing a cookie to an authenticated user or denying an invalid login.
+        ///// </summary>
+        ///// <param name="loginRequest">Wrapper for URL parameters (Username, PasswordHash).</param>
+        ///// <returns>Action based on result of authentication</returns>
+        //public HttpStatusCodeResult Login(LoginRequest loginRequest)  // returning status code for testing purposes
+        //// public ActionResult Login(LoginRequest loginRequest)
+        //{
+        //    // TODO: redirect to proper page
+
+        //    string username = loginRequest.Username;
+        //    string passwordHash = loginRequest.PasswordHash;
+
+        //    try
+        //    {
+        //        if (String.IsNullOrWhiteSpace(username) || String.IsNullOrWhiteSpace(passwordHash))
+        //        {
+        //            // error: invalid parameter
+        //            throw new ApplicationException("Value username and/or passwordHash is invalid!");
+        //        }
+
+        //        using (var db = new LibraryEntities())
+        //        {
+        //            var targetLogin = db.Logins.Where(x => x.Username == username).FirstOrDefault();
+        //            if (targetLogin == null)
+        //            {
+        //                // error: user not found
+        //                throw new ApplicationException("User " + username + " not found!");
+        //            }
+
+        //            bool isCorrectPassword = targetLogin.PasswordHash == passwordHash;
+        //            if (!isCorrectPassword)
+        //            {
+        //                // error: incorrect password
+        //                throw new ApplicationException("Incorrect password!");
+        //            }
+
+        //            HttpCookie loginCookie = CreateCookie(targetLogin);
+        //            Response.Cookies.Add(loginCookie);
+
+        //            //return View();
+        //            return new HttpStatusCodeResult(200);
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        // handle login error
+        //        return new HttpStatusCodeResult(500);
+        //    }
+        //}
+
+        //private HttpCookie CreateCookie(Login login)
+        //{
+        //    string cookieValue = String.Format("{0};{1};{2}", login.AccountId, login.LoginId, login.Username);
+        //    string cookieHash;
+
+        //    using (var sha512 = SHA512.Create())
+        //    {
+        //        byte[] cookieBytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(cookieValue));
+        //        var sb = new StringBuilder();
+        //        foreach (byte b in cookieBytes)
+        //        {
+        //            sb.Append(b.ToString("x2"));
+        //        }
+        //        cookieHash = sb.ToString();
+        //    }
+
+        //    var cookie = new HttpCookie("auth", login.Username + ":" + cookieHash)
+        //    {
+        //        Expires = DateTime.Now.AddDays(1)
+        //    };
+
+        //    return cookie;
+        //}
     }
 }
