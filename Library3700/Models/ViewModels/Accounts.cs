be824265d7a2572aaff8e25601621d6104955485
ThaddeusHatcher <thaddeusrhatcher@gmail.com>
2018-04-29 19:30:40 -0500

    stuff for Librarian Report

---
+++ Library3700/Models/ViewModels/Accounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library3700.Models.ViewModels
+{
+    public class Accounts
+    {
+        public Nullable<int> AccountID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsLibrarian { get; set; }
+        public AccountStatusType AccountStatus { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
